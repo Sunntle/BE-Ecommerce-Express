ca43470d0d41615fe9892a912fffecf55baf4bc0
Sunntle <taile2608@gmail.com>
2023-12-26 19:28:30 +0700

    Add handle not found

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,16 @@ const initRoutes = (app) =>{
     app.use("/product_colors", productColorRouter);
     app.use("/product_sizes", productSizesRouter);
     app.use("/images", imagesRouter);
+    app.use((req, res, next) => {
+        const err = new Error(`Route ${req.originalUrl} not found !`);
+        res.status(404)
+        next(err)
+    }, (err, req, res, next) => {
+        const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+        return res.status(statusCode).json({
+            success: false,
+            message: err.message
+        })
+    })
 }
 module.exports = initRoutes
