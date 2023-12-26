7615b9f7ba36831a4ee14ac39ae2b1b9644995d0
Sunntle <taile2608@gmail.com>
2023-12-26 14:22:00 +0700

    add config webpack

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin')
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
+    clean: true,
+  },
+  target: "node",
+  mode: 'production',
+  resolve: {
+    extensions: [".js"],
+    fallback: {
+        "mock-aws-s3": false,
+        "pg-hstore": false,
+        "nock": false,
+        "aws-sdk": false,
+      },
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+    ],
+  },
+};
